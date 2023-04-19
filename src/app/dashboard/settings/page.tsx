4fea2a32c664e6bcbf0815f7a20a6f91cbd6b1c7
Cristina Zamora <ecoandrea@gmail.com git config --global user.nama CriszDev git config --global user.name CriszDevgit config --global user.email ecoandrea@gmail.com>
2023-04-19 01:10:52 -0400

    dia 1

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,8 @@
+
+const PageSettings = () => {
+  return (
+    <div>PageSettings</div>
+  )
+}
+
+export default PageSettings
---
+++ src/components/Counter.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState } from "react"
+import ButtonCounter from "./ButtonCounter"
+
+
+export default function Counter()  {
+
+        const [count, setCount] = useState(0)
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <ButtonCounter 
+            setCount={setCount} 
+            count={count}
+            
+            />
+        </div>
+    )
+}
+
